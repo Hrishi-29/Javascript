e420d951acde71db9f6633ad56f643b4128a2559
Hrishi Raj Verma <vhraj1629@gmail.com>
2026-02-19 19:46:14 +0530

    3rd Qs done

--- assignsPractice/destructuringArrays.js
+++ assignsPractice/destructuringArrays.js
@@ -8,3 +8,11 @@ const [firstBook, secondBook] = books;
 //Destructure the books array into a variable called thirdBook. You must skip the first two books
 const [, , thirdBook] = books;
 // console.log(thirdBook);
+
+// destructuring, the ratings variable should store a number 4.19, and the ratingsCount variable should store a number 144584
+const ratings = [
+    ["rating", 4.19],
+    ["ratingsCount", 144584],
+];
+const [[, rating], [, ratingsCount]] = ratings;
+// console.log(rating, ratingsCount);
