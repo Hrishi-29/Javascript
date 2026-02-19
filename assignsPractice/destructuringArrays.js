76469e94883cd908b96d2c2b91c81abbdd9e12c6
Hrishi Raj Verma <vhraj1629@gmail.com>
2026-02-19 15:23:17 +0530

    2nd Qs done

--- assignsPractice/destructuringArrays.js
+++ assignsPractice/destructuringArrays.js
@@ -4,3 +4,7 @@ const books = require("./booksInfo");
 // Destructure the books array into two variables called firstBook and secondBook
 const [firstBook, secondBook] = books;
 // console.log(firstBook, secondBook);
+
+//Destructure the books array into a variable called thirdBook. You must skip the first two books
+const [, , thirdBook] = books;
+// console.log(thirdBook);
