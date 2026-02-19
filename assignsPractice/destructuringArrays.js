c3ad09c85a56c9f3a85d66737a3016c37072d417
Hrishi Raj Verma <vhraj1629@gmail.com>
2026-02-19 15:19:11 +0530

    1st Qs done

--- assignsPractice/destructuringArrays.js
+++ assignsPractice/destructuringArrays.js
@@ -1,2 +1,6 @@
 "use strict";
 const books = require("./booksInfo");
+
+// Destructure the books array into two variables called firstBook and secondBook
+const [firstBook, secondBook] = books;
+// console.log(firstBook, secondBook);
