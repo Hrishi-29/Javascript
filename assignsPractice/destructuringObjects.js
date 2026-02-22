174122ab427ea5d0ece178f782d6e4fa26a7cd4d
Hrishi Raj Verma <vhraj1629@gmail.com>
2026-02-22 18:36:02 +0530

    1st Qs done

--- assignsPractice/destructuringObjects.js
+++ assignsPractice/destructuringObjects.js
@@ -1,2 +1,6 @@
 "use strict";
 const books = require("./booksInfo");
+
+// Destructure the first book object from the books array into variables called title, author and ISBN
+const { title, author, ISBN } = books[0];
+// console.log(title, author, ISBN);
