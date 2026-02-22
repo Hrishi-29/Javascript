2fdd63e0ae29e9e69b0d16fcd66a98071daa3351
Hrishi Raj Verma <vhraj1629@gmail.com>
2026-02-22 18:59:37 +0530

    3rd Qs done

--- assignsPractice/destructuringObjects.js
+++ assignsPractice/destructuringObjects.js
@@ -8,3 +8,7 @@ const { title, author, ISBN } = books[0];
 // Destructure the first book object from the books array into a variable called tags. The tags variable should be assigned with the value of the keywords property
 const { keywords: tags } = books[0];
 // console.log(tags);
+
+// Destructure the seventh book object (books[6]) into variables called language and programmingLanguage. Assign the programmingLanguage variable with a default value of 'unknown'
+const { language, programmingLanguage = "unknown" } = books[6];
+// console.log(language, programmingLanguage);
