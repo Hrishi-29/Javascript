66c942f15e4e57eb0b8b270a93374ba1aa2b4747
Hrishi Raj Verma <vhraj1629@gmail.com>
2026-02-22 18:43:23 +0530

    2nd Qs done

--- assignsPractice/destructuringObjects.js
+++ assignsPractice/destructuringObjects.js
@@ -4,3 +4,7 @@ const books = require("./booksInfo");
 // Destructure the first book object from the books array into variables called title, author and ISBN
 const { title, author, ISBN } = books[0];
 // console.log(title, author, ISBN);
+
+// Destructure the first book object from the books array into a variable called tags. The tags variable should be assigned with the value of the keywords property
+const { keywords: tags } = books[0];
+// console.log(tags);
