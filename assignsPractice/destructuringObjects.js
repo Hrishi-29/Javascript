d391655ce232feb7bb84900ce8e7960d9cbb9c02
Hrishi Raj Verma <vhraj1629@gmail.com>
2026-05-25 15:14:34 +0530

    beautify my codes

--- assignsPractice/destructuringObjects.js
+++ assignsPractice/destructuringObjects.js
@@ -2,21 +2,39 @@
 const books = require("./booksInfo");
 
 // Destructure the first book object from the books array into variables called title, author and ISBN
-const { title, author, ISBN } = books[0];
+const {
+    title,
+    author,
+    ISBN
+} = books[0];
 // console.log(title, author, ISBN);
 
 // Destructure the first book object from the books array into a variable called tags. The tags variable should be assigned with the value of the keywords property
-const { keywords: tags } = books[0];
+const {
+    keywords: tags
+} = books[0];
 // console.log(tags);
 
 // Destructure the seventh book object (books[6]) into variables called language and programmingLanguage. Assign the programmingLanguage variable with a default value of 'unknown'
-const { language, programmingLanguage = "unknown" } = books[6];
+const {
+    language,
+    programmingLanguage = "unknown"
+} = books[6];
 // console.log(language, programmingLanguage);
 
 // Below are two variables called booktitle and bookAuthor. Reassign them with the values of the title and author properties of the first book object from the books array.
-const{title: bookTitle ,author: bookAuthor} = books[0];
+const {
+    title: bookTitle,
+    author: bookAuthor
+} = books[0];
 // console.log(title, author);
 
 // Destructure the first book object from the books array into a variable called bookRating. In result, the bookRating variable should be assigned with the value of the book[0].thirdParty.goodreads.rating properties.
-const {thirdParty: {goodreads: {rating: bookRating}}} = books[0];
+const {
+    thirdParty: {
+        goodreads: {
+            rating: bookRating
+        }
+    }
+} = books[0];
 // console.log(bookRating);
