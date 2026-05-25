fd50a62c4e8b92734f47468ec267a5db120af983
Hrishi Raj Verma <vhraj1629@gmail.com>
2026-05-25 21:21:45 +0530

    two Qs done

--- assignsPractice/spreadOperator.js
+++ assignsPractice/spreadOperator.js
@@ -1,2 +1,12 @@
 'use strict';
 const books = require('./booksInfo');
+
+// Declare an array called bookAuthors(no nested array), and fill it with authors of the first two books from the books array.
+const bookAuthors = [...books[0].author, ...books[1].author]
+// console.log(bookAuthors)
+
+// Write a function called spellWord that accepts a single string as an argument. This function should log to the console each letter of the argument seperated by a space.
+function spellWord(letters){
+    // console.log(...letters);
+}
+spellWord('Javascript');
