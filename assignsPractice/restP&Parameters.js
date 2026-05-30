1de002f941551d4bb94df9038f26f344ae3f7c47
Hrishi Raj Verma <vhraj1629@gmail.com>
2026-05-30 18:13:57 +0530

    3rd Qs done

--- assignsPractice/restP&Parameters.js
+++ assignsPractice/restP&Parameters.js
@@ -8,3 +8,10 @@ const [mainKeyword, ...rest] = books[0].keywords;
 // The bookPublisher variable should be assigned with the value of the publisher property of the second book object. Assign rest of the properties to the restofTheBook variable.
 const { publisher: bookPublisher, ...restofTheBook} = books[1]  ;
 // console.log(bookPublisher, restofTheBook);
+
+// Write a function printBookAuthorsCount that has title and authors parameters which authors should accept numbers and expected output:'The book "Algorithms" has 2 authors'.
+function printBookAuthorsCount(title, ...authors){
+    // console.log(`The book "${title}" has ${authors.length} authors`);
+};
+const {author: names} = books[0];
+printBookAuthorsCount('Algorithms', ...names);
