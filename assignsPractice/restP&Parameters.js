8df51ec01c8dec98663b7a932764311208ecb9e0
Hrishi Raj Verma <vhraj1629@gmail.com>
2026-05-30 17:44:47 +0530

    2nd Qs done

--- assignsPractice/restP&Parameters.js
+++ assignsPractice/restP&Parameters.js
@@ -4,3 +4,7 @@ const books = require('./booksInfo');
 // Destruct the first keyword of the first book should be assigned to mainkeyword, and the rest of the keywords should be assigned to the rest variable.
 const [mainKeyword, ...rest] = books[0].keywords;
 // console.log(mainKeyword, rest);
+
+// The bookPublisher variable should be assigned with the value of the publisher property of the second book object. Assign rest of the properties to the restofTheBook variable.
+const { publisher: bookPublisher, ...restofTheBook} = books[1]  ;
+// console.log(bookPublisher, restofTheBook);
