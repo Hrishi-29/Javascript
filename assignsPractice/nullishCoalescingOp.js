924195f6d68ab7400b2bf7194e6d930d48c2f3c2
Hrishi Raj Verma <vhraj1629@gmail.com>
2026-05-31 22:14:07 +0530

    nullishOp Qs done

---
+++ assignsPractice/nullishCoalescingOp.js
@@ -0,0 +1,8 @@
+'use strict';
+const books = require('./booksInfo');
+
+// Loop over the books array, and for the books that provide online content, log "${title}" provides no data about its online content.
+
+for(var i = 0, length1 = books.length; i < length1; i++){
+    // books[i].onlineContent ?? console.log(`"${books[i].title}" provides no data about its online content.`)
+}
