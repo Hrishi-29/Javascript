35857a9842f632c4d891ff49bab8ae71be478af0
Hrishi Raj Verma <vhraj1629@gmail.com>
2026-06-02 15:21:31 +0530

    2nd Qs done

--- assignsPractice/logicalAssignOp.js
+++ assignsPractice/logicalAssignOp.js
@@ -6,3 +6,9 @@ for(var i = 0, length1 = books.length; i < length1; i++){
     books[i].edition??=1;
     // console.log(books[i]);
 }
+
+// Iterate over the books array, and if the thirdParty.goodreads.rating property is less than 4.2 then reassign highlighted property is false
+for(var i = 0, length1 = books.length; i < length1; i++){
+    books[i].highlighted&&=!(books[i].thirdParty.goodreads.rating<4.2);
+    // console.log(books[i]);
+}
