d3f5b72faf1c0ea1490e964bead4e5439a68b5e0
Hrishi Raj Verma <vhraj1629@gmail.com>
2026-06-05 13:27:52 +0530

    2nd Qs done

--- assignsPractice/loopingArrays.js
+++ assignsPractice/loopingArrays.js
@@ -4,7 +4,22 @@ const books = require('./booksInfo');
 // Use the for-of loop to loop over the books array and sum the pages of all books.
 let pageSum = 0;
 for (const book of books) {
-    const {pages: num} = book;
+    const {
+        pages: num
+    } = book;
     pageSum += num;
 }
 // console.log(pageSum);
+
+// Use the for-of loop to fill allAuthors variables which stores an empty array with the authors of each books from the books array.
+const allAuthors = [];
+for (const book of books) {
+    if (typeof book.author) {
+        allAuthors.push(book.author);
+    } else {
+        for (const author of book.author) {
+            allAuthors.push(book.author);
+        }
+    }
+}
+// console.log(allAuthors)
