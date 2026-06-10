da49e81a2f064f2deb2557780ba98e24372e6c96
Hrishi Raj Verma <vhraj1629@gmail.com>
2026-06-10 13:56:13 +0530

    prev Qs update

--- assignsPractice/loopingObjects.js
+++ assignsPractice/loopingObjects.js
@@ -1,8 +1,9 @@
 'use strict';
 const books = require('./booksInfo');
 
+// Use the for-of loop together with the Object.keys() method to loop over the thirdParty.goodreads property(array) of the first book object from books array and push to the entries array
 const entries = [];
-for (let key of Object.keys(books[0].thirdParty.goodreads)) {
+for (const key of Object.keys(books[0].thirdParty.goodreads)) {
     entries.push([key]);
 }
 // console.log(entries);
