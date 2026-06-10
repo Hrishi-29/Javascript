10b4c9d48dd0ff4b1d39656ea2c3ed44f381a54d
Hrishi Raj Verma <vhraj1629@gmail.com>
2026-06-10 17:54:06 +0530

    2nd Qs done

--- assignsPractice/loopingObjects.js
+++ assignsPractice/loopingObjects.js
@@ -7,3 +7,9 @@ for (const key of Object.keys(books[0].thirdParty.goodreads)) {
     entries.push([key]);
 }
 // console.log(entries);
+
+// Use the for-of loop together with the Object.values() method and the Object.entries() method to loop over the thirdParty.goodreads of first book Object from the books array and push in the entries array(use index from entries()).
+for (const [num, key] of Object.values(books[0].thirdParty.goodreads).entries()) {
+    entries[num].push(key);
+}
+// console.log(entries)
