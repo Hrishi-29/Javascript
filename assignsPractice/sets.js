cabf1ed3c5d9a6176b663e1b03a9980f295f550d
Hrishi Raj Verma <vhraj1629@gmail.com>
2026-06-14 15:18:03 +0530

    without_using_sets 1st Qs done

--- assignsPractice/sets.js
+++ assignsPractice/sets.js
@@ -4,12 +4,12 @@ const books = require('./booksInfo');
 // Fill the allKeywords which is empty array with the keywords property from the books arrays of each book object and no nested array contain.
 const allKeywords = [];
 
-const sets = new Set(allKeywords);
+// const sets = new Set(allKeywords);
 for (const book of books) {
-    sets.add(book.keywords);
+    allKeywords.push(...book.keywords);
 }
-const keywords = new Set(...sets);
-for (const keyword of keywords) {
-    allKeywords.push(keyword);
-}
+// const keywords = new Set(...sets);
+// for (const keyword of keywords) {
+//     allKeywords.push(keyword);
+// }
 // console.log(allKeywords)
