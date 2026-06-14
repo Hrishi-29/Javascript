520412231c4a63a8e708a62ab346ccf461721721
Hrishi Raj Verma <vhraj1629@gmail.com>
2026-06-14 17:57:33 +0530

    mapFundamental Qs

---
+++ assignsPractice/mapsBasics.js
@@ -0,0 +1,6 @@
+'use strict';
+const books = require('./booksInfo');
+
+// Create a map with the name of bookMap and insert a data.
+const bookMap = new Map([['title', 'Clean Code'], ['author', 'Robert C. Martin']]);
+// console.log(bookMap)
