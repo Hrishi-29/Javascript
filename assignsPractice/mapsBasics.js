719ede7ebff2e014ad0e3a0bab624ae66900a05c
Hrishi Raj Verma <vhraj1629@gmail.com>
2026-06-14 18:29:57 +0530

    4th Qs done

--- assignsPractice/mapsBasics.js
+++ assignsPractice/mapsBasics.js
@@ -6,7 +6,10 @@ const bookMap = new Map([['title', 'Clean Code'], ['author', 'Robert C. Martin']
 // console.log(bookMap)
 
 // Set pages a new key and assign 464 number in bookMap.
-console.log(bookMap.set('pages', '464'))
+// console.log(bookMap.set('pages', '464'))
 
 // Get the title and author value from bookMap and console it in a string.
 // console.log(`${bookMap.get('title')} by ${bookMap.get('author')}`)
+
+// Get the size of bookMap and log it.
+// console.log(bookMap.size)
