2902f054ec95eaf85d1cc150dd4ae2d1426b6aed
Hrishi Raj Verma <vhraj1629@gmail.com>
2026-06-14 19:08:57 +0530

    2nd Qs done

--- assignsPractice/mapsItr.js
+++ assignsPractice/mapsItr.js
@@ -4,3 +4,7 @@ const books = require('./booksInfo');
 // Convert the first book object from the books array into a Map and assign it in firstBookMap variable.
 const firstBookMap = new Map(Object.entries(books[0]));
 // console.log(firstBookMap)
+
+for (const [keys, value] of firstBookMap) {
+    if(typeof value === 'number') console.log(keys)
+}
