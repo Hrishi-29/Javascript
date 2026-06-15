c263757e430a351fd03146a1db6460def565dd4b
Hrishi Raj Verma <vhraj1629@gmail.com>
2026-06-15 15:18:40 +0530

    challenging Qs

--- assignsPractice/codingChallenge.js
+++ assignsPractice/codingChallenge.js
@@ -92,3 +92,23 @@ for (let name of Object.values(game.scored)) {
     scorers[name]++ || (scorers[name] = 1);
 }
 // console.log(scorers)
+
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+const events = new Set();
+for (const [key, value] of gameEvents) {
+    events.add(value);
+}
+// console.log([...events])
